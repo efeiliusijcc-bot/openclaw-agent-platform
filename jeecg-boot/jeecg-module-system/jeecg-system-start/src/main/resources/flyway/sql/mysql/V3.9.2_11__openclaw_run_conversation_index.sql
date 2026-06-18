bc88451cdbebf8f50b9542450ef8d53da96a1cc1
baby sam <efeiliusijcc@gmail.com>
2026-06-18 16:36:50 +0800

    feat(openclaw): index run conversations

---
+++ jeecg-boot/jeecg-module-system/jeecg-system-start/src/main/resources/flyway/sql/mysql/V3.9.2_11__openclaw_run_conversation_index.sql
@@ -0,0 +1,1 @@
+CREATE INDEX idx_openclaw_run_conversation_time ON openclaw_agent_run (conversation_id, create_time);
---
+++ jeecg-boot/jeecg-module-system/jeecg-system-start/src/main/resources/flyway/sql/postgresql/V3.9.2_11__openclaw_run_conversation_index.sql
@@ -0,0 +1,1 @@
+CREATE INDEX IF NOT EXISTS idx_openclaw_run_conversation_time ON openclaw_agent_run (conversation_id, create_time);
